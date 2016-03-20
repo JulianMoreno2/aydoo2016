1732bbb23b0a9e86caf041ba6db26d0d6acdb5da
juliani2 <julianezequielmoreno@gmail.com>
2016-03-20 20:15:47 -0300

    corrijo la disposicion de los numeros que se imprimian en pantalla

--- factores-primos/src/main/java/ar/edu/untref/aydoo/FactoresPrimos.java
+++ factores-primos/src/main/java/ar/edu/untref/aydoo/FactoresPrimos.java
@@ -19,17 +19,17 @@ public class FactoresPrimos {
 		{
 			if((this.numero%5) == 0)
 			{			
-				System.out.println("5");
+				System.out.print(" 5 ");
 				this.numero = this.numero/5;				
 			
 			}else if((this.numero%3) == 0)
 			{
-				System.out.println("3");
+				System.out.print(" 3 ");
 				this.numero = this.numero/3;
 			
 			}else if((this.numero%2) == 0)
 			{
-				System.out.println("2");
+				System.out.print(" 2 ");
 				this.numero = this.numero/2;
 			}
 		}
@@ -39,7 +39,8 @@ public class FactoresPrimos {
 	 {
 		 FactoresPrimos factorPrimo = new FactoresPrimos(Integer.parseInt(arg[0]));
 		 
-		 System.out.println("Factores primos de: "+factorPrimo.getNumero());
+		 System.out.print("Factores primos de "+factorPrimo.getNumero()+":");
 		 factorPrimo.calcularFactoresPrimos();
+		 System.out.println("");
 	 }
 }
