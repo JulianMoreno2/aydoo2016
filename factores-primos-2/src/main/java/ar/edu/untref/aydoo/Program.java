2c1e5cae8eb920567b7173a1bd285648713c3d7a
juliani2 <julianezequielmoreno@gmail.com>
2016-03-26 22:37:26 -0300

    corrijo ejercicio factores primos 2

--- factores-primos-2/src/main/java/ar/edu/untref/aydoo/Program.java
+++ factores-primos-2/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -3,21 +3,21 @@ package ar.edu.untref.aydoo;
 public class Program {
 
 	 public static final void main(String arg[])
-	 {
+	 {	
 		 FactoresPrimos factorPrimo = new FactoresPrimos(Integer.parseInt(arg[0]));
 		 
 		 String formato = arg[1].toLowerCase();
 		 
-		 if(formato == "--format=pretty"){
-
+		 if(formato.equals("--format=pretty")){
+		
 			 System.out.print("Factores primos de "+factorPrimo.getNumero()+": ");
-			 
+		 			 
 			 for(int i = 0; i < factorPrimo.calcularFactoresPrimos().size();i++){
 				 System.out.print(factorPrimo.calcularFactoresPrimos().get(i)+" ");
 			 }
 			 System.out.println("");			 
 		 
-		 }else if(formato == "--format=quiet"){
+		 }else if(formato.equals("--format=quiet")){
 							 
 			 for(int i = 0; i < factorPrimo.calcularFactoresPrimos().size();i++){
 				 System.out.println(factorPrimo.calcularFactoresPrimos().get(i)+" ");
@@ -25,8 +25,10 @@ public class Program {
 			 System.out.println("");
 			 
 		 }else{
+		
 			 System.out.println("Formato no aceptado. Las opciones posibles son : pretty o quiet");
 		 }
+		
 	 }
 
 }
