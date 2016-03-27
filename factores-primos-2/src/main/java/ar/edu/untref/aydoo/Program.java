4486cc84088065bd824375d0327f88e027f05e77
juliani2 <julianezequielmoreno@gmail.com>
2016-03-27 17:25:21 -0300

    corrijo ejercicio factores primos 2 array of bounds exception

--- factores-primos-2/src/main/java/ar/edu/untref/aydoo/Program.java
+++ factores-primos-2/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -4,31 +4,47 @@ public class Program {
 
 	 public static final void main(String arg[])
 	 {	
-		 FactoresPrimos factorPrimo = new FactoresPrimos(Integer.parseInt(arg[0]));
-		 
-		 String formato = arg[1].toLowerCase();
-		 
-		 if(formato.equals("--format=pretty")){
-		
-			 System.out.print("Factores primos de "+factorPrimo.getNumero()+": ");
-		 			 
-			 for(int i = 0; i < factorPrimo.calcularFactoresPrimos().size();i++){
-				 System.out.print(factorPrimo.calcularFactoresPrimos().get(i)+" ");
+		 try
+		 {			 
+			 FactoresPrimos factorPrimo = new FactoresPrimos(Integer.parseInt(arg[0]));
+
+			 if(arg.length == 1){
+				 
+				 System.out.print("Factores primos de "+factorPrimo.getNumero()+": ");
+				 
+				 for(int i = 0; i < factorPrimo.calcularFactoresPrimos().size();i++){
+					 System.out.print(factorPrimo.calcularFactoresPrimos().get(i)+" ");
+				 }
+				 System.out.println("");	
+				 
+				 
+			 }else if (arg.length == 2){
+	
+				 if(arg[1].toLowerCase().equals("--format=pretty") || arg[1].toLowerCase() == null){
+					 
+					 System.out.print("Factores primos de "+factorPrimo.getNumero()+": ");
+					 
+					 for(int i = 0; i < factorPrimo.calcularFactoresPrimos().size();i++){
+						 System.out.print(factorPrimo.calcularFactoresPrimos().get(i)+" ");
+					 }
+					 System.out.println("");			 
+					 
+				 }else if(arg[1].toLowerCase().equals("--format=quiet")){
+					 
+					 for(int i = factorPrimo.calcularFactoresPrimos().size()-1; i >= 0 ;i--){
+						 System.out.println(factorPrimo.calcularFactoresPrimos().get(i)+" ");
+					 }
+					 System.out.println("");
+					 
+				 }else{
+					 
+					 System.out.println("Formato no aceptado. Las opciones posibles son : pretty o quiet");
+				 }
 			 }
-			 System.out.println("");			 
-		 
-		 }else if(formato.equals("--format=quiet")){
-							 
-			 for(int i = 0; i < factorPrimo.calcularFactoresPrimos().size();i++){
-				 System.out.println(factorPrimo.calcularFactoresPrimos().get(i)+" ");
-			 }
-			 System.out.println("");
 			 
-		 }else{
-		
+		 }catch(ArrayIndexOutOfBoundsException e){
+			 
 			 System.out.println("Formato no aceptado. Las opciones posibles son : pretty o quiet");
 		 }
-		
 	 }
-
 }
