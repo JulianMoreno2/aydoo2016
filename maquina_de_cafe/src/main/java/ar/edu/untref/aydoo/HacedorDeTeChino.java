4cf332ea22789c228b66c8b83fea98727f19a5e5
juliani2 <julianezequielmoreno@gmail.com>
2016-03-28 01:09:59 -0300

    agrego funcionalidad a program

---
+++ maquina_de_cafe/src/main/java/ar/edu/untref/aydoo/HacedorDeTeChino.java
@@ -0,0 +1,11 @@
+package ar.edu.untref.aydoo;
+
+public class HacedorDeTeChino {
+	
+	public void prepararEnEsteVaso(Vaso vaso) {
+
+		System.out.println("Hacedor de Te Chino: pongo te en el vaso.");
+		vaso.setContenido("Te chino");
+		System.out.println("Hacedor de Te: devolver vaso.");
+	}
+}
--- maquina_de_cafe/src/main/java/ar/edu/untref/aydoo/Program.java
+++ maquina_de_cafe/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -4,6 +4,35 @@ public class Program {
 
 	 public static final void main(String arg[])
 	 {
+		 if(arg.length == 1){
+			 
+			 System.out.println("Tienes que especificar la cantidad de cucharadas de azucar que desea");
+			 
+		 }else if(arg.length == 2){
+			 
+			 MaquinaExpendedoraDeBebidas maquina = new MaquinaExpendedoraDeBebidas();
+			 
+			 if(arg[0].equals("hacerCafeConLecheConNdeAzucar:")){
+			
+				 maquina.hacerCafeConLecheConNDeAzucar(Integer.parseInt(arg[1]));				 
+			 
+			 }else if(arg[0].equals("hacerTeConLecheConNdeAzucar:")){
+				 
+				 maquina.hacerTeConLecheConNDeAzucar(Integer.parseInt(arg[1]));
+			 
+			 }else{
+				 
+				 System.out.println("Las opciones validas son:");
+				 System.out.println("hacerCafeConLecheConNdeAzucar: n");
+				 System.out.println("hacerCafeConLecheConNdeAzucar: n");
+			 }
+			 
+		 }else{
+			 
+			 System.out.println("Las opciones validas son:");
+			 System.out.println("hacerCafeConLecheConNdeAzucar: n");
+			 System.out.println("hacerCafeConLecheConNdeAzucar: n");
+		 }
 		 
 	 }
 
